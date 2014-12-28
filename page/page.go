19ee73a8dce51399611eff24cf63fdf616883276
mewmew <rnd0x00@gmail.com>
2014-12-28 10:46:00 +0100

    page: Add review comments.

--- page/page.go
+++ page/page.go
@@ -32,9 +32,33 @@ type Page struct {
 	Settings settings.Page
 }
 
+// NOTE: As filename.Encode is always used in combination with UrlAsFilename,
+// move the Encode function call into the defintion of UrlAsFilename. Make sure
+// to update the call locations in both page/page.go and
+// cmd/nyfikend/nyfikend.go.
+
+// NOTE: golint would complain about Url and recommend using URL instead as it
+// is an acronym. If you agree with this advice is entirely up to you.
+
+// NOTE: Make sure that UrlAsFilename always returns a unique representation.
+// E.g. are these URLs encoded differently?
+//    foo.in/fobar
+//    foo.info/bar
+
+// UrlAsFilename returns a unique representation of the page's URL with illegal
+// filesystem characters encoded.
 func (p *Page) UrlAsFilename() string {
 	return p.ReqUrl.Host + p.ReqUrl.Path + p.ReqUrl.RawQuery
 }
+
+// NOTE: Avoid concurrency in the exposed API [1]. As some of the packages of
+// nyfikend are quite tightly integrated this may not be an issue, where the
+// concurrent version of Check works as a helper function. For standalone
+// packages concurrency should be avoided in the API, as clients of the API may
+// want to use serial execution and executing a non-concurrent function
+// concurrently is easy.
+//
+// [1] http://talks.golang.org/2013/bestpractices.slide#25
 
 // Check downloads and makes a specialized comparison with a previous check
 // saved on disk to determine if the page has been updated. Check takes
@@ -43,17 +67,31 @@ func (p *Page) Check(ch chan<- error) {
 	ch <- p.check()
 }
 
+// NOTE: The check function implements a lot of functionality and is massive
+// (more than 160 lines). Consider factoring out some functionality to dedicated
+// functions; for instance the sending of email notifications. Generally a
+// function should fit on a screen or two; anything more will make it difficult
+// to keep it all in your head.
+
 // check is an non-exported function for better error handling.
 func (p *Page) check() (err error) {
 	if settings.Verbose {
 		fmt.Println("[/] Downloading:", p.ReqUrl.String())
 	}
 
+	// NOTE: Use embedding to propagate methods of a given struct member, not to
+	// save key strokes. As *html.Node and error are embedded members of r the
+	// struct will have the Error, AppendChild, InsertBefore and RemoveChild
+	// methods.
+	//
+	//    var r struct { node *html.Node; err error }
+
 	// Retrieve result from download or return timeout error.
 	var r struct {
 		*html.Node
 		error
 	}
+	// NOTE: Ideomatic use of select and time.After for timeouts, nice :)
 	select {
 	case r = <-errWrapDownload(p):
 		if r.error != nil {
@@ -69,6 +107,9 @@ func (p *Page) check() (err error) {
 		return errutil.Err(err)
 	}
 
+	// NOTE: Rename linuxPath to pagePath or something, as nyfiken may be running
+	// on other operating systems.
+
 	// Filename is the URL encoded and the protocol is stripped.
 	linuxPath, err := filename.Encode(p.UrlAsFilename())
 	if err != nil {
@@ -81,11 +122,19 @@ func (p *Page) check() (err error) {
 		return errutil.Err(err)
 	}
 	// Update the debug comparison file.
+
+	// NOTE: Consider using filepath.Join instead of string appends. The ".htm"
+	// string still needs to be appended as it is part of the filename and not a
+	// distinct directory path. E.g.
+	//
+	//    debugCachePathName := filepath.Join(settings.DebugCacheRoot, linuxPath + ".htm")
 	debugCachePathName := settings.DebugCacheRoot + linuxPath + ".htm"
 	err = ioutil.WriteFile(debugCachePathName, []byte(debug), settings.Global.FilePerms)
 	if err != nil {
 		return errutil.Err(err)
 	}
+
+	// NOTE: This check has saved me quite a few times :)
 
 	// If the selection is empty, the CSS selection is probably wrong so we will
 	// alert the user about this problem.
@@ -194,6 +243,10 @@ func (p *Page) check() (err error) {
 	return nil
 }
 
+// NOTE: Consider adding an unexported type name for struct{*html.Node;error} as
+// it is used several times, and if so make sure to update both check and
+// errWrapDownload.
+
 // An error wrapping convenience function for p.download() used because of
 // timeout implementation.
 // Credits to: Dave Cheney and ilyia (https://groups.google.com/forum/?fromgroups=#!topic/golang-nuts/cTrBcyjqCxg)
@@ -223,6 +276,8 @@ func (p *Page) download() (doc *html.Node, err error) {
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
+
+	// NOTE: Simple and clean way of adding custom HTTP headers to a request.
 
 	// If special headers were specified, add them to the request.
 	if p.Settings.Header != nil {
@@ -264,15 +319,26 @@ func (p *Page) download() (doc *html.Node, err error) {
 			keyval := strings.Split(typ, `=`)
 			if len(keyval) == 2 {
 				charset = keyval[1]
+				// NOTE: I've added a break for when the first charset is found. If
+				// the Content-Type contains multiple charsets this may not be
+				// wanted. What do you think is the right behaviour?
+				break
 			}
 		}
 	}
 	if charset != "utf-8" {
+		// NOTE: I like the fact that you handle non UTF-8 encodings! I've not
+		// come across any encoding issues while running nyfiken, maybe this is
+		// why.
 		content = mahonia.NewDecoder(charset).ConvertString(content)
 	}
 	// Parse response into html.Node.
 	return html.Parse(strings.NewReader(content))
 }
+
+// NOTE: Definitely break the makeSelection function into smaller functions.
+// Right now you are using --- [ foo ] --- to separate the functionality, so
+// split it instead.
 
 // Select from the retrived page source the CSS selection defined in c4c.ini.
 func (p *Page) makeSelection(htmlNode *html.Node) (selection string, err error) {
@@ -315,6 +381,13 @@ func (p *Page) makeSelection(htmlNode *html.Node) (selection string, err error) 
 		if err != nil {
 			return "", errutil.Err(err)
 		}
+		// NOTE: This is one of the features I enjoy the most with nyfiken! The
+		// strip functions are simply great. Consider adding a default case which
+		// catches invalid or misspelled strip functions. I know that the
+		// nyfiken/ini package already takes care of this, but a future version of
+		// nyfiken may replace the configuration implementation and forget this
+		// check. In general, use defence in depth when checking for errors. I
+		// know this is a silly example but the principle is the same.
 		stripFunc = strings.ToLower(stripFunc)
 		switch stripFunc {
 		case "numbers":
