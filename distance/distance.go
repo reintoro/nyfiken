7c9dd1bbe826b9457cc8e5294627be94ad30076d
mewmew <rnd0x00@gmail.com>
2014-12-28 13:42:29 +0100

    distance: Add review comment.

--- distance/distance.go
+++ distance/distance.go
@@ -1,6 +1,13 @@
 // Package distance measures the distances between two strings.
 package distance
 
+// NOTE: The ad-hoc function works well enough but it would be interesting to
+// investigate other string distance functions. If it could consider "helol" and
+// "hello" to be more similar than "hello" and "loleh" that would be great, thus
+// differentiating between simple typos and distinct words. This could be a
+// don't fix it if it ain't broken, but in the name of for fun and profit
+// anything is fair game :)
+
 // An ad-hoc function for a percentage difference between two strings.
 func Approx(str1, str2 string) float64 {
 	var sum1 float64
